dac9253cccf37a1786bdf9594f18559e7a96f0da
ayush-dev-jpg <ayushgithub90@gmail.com>
2025-02-28 15:19:23 +0000

    datatypes-summary incomp

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,8 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null(not Zero), undfined, Symbol(to make the value unique), BigInt
+
+
+// Reference(Non-Primitive)
+
+//Array, Objects, Functions
