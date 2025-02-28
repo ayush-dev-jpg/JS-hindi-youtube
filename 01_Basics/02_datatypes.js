b74bce9277e7056c0d0d675ed2589abcd1afa576
ayush-dev-jpg <ayushgithub90@gmail.com>
2025-02-28 13:51:37 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello"); //We are using node js not browser
+
+//Code readability should be high
+
+let name = "ayush"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value has not been assigne yet
+//symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); //(interview Q)object => Now this is controversial because some people say that this a langauge barrier whereas other say that its correct
+console.log(typeof undefined); //undefined
