66f825ae058d24e4da1b699e5cb84e10b9fc5f9f
ayush-dev-jpg <ayushgithub90@gmail.com>
2025-02-28 14:16:18 +0000

    Conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" gets easily converted to 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "Radhe Radhe"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh" => true
+
+let someNumber = undefined
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
