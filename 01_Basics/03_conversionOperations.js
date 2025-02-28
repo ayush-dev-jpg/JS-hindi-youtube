832300a728ef71fc90fe43c9d2d3fc5516785f0a
ayush-dev-jpg <ayushgithub90@gmail.com>
2025-02-28 14:46:27 +0000

    Updated Operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -4,8 +4,8 @@ let score = "abc";
 // console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" gets easily converted to 33
 //"33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Radhe Radhe"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
@@ -23,6 +23,37 @@ console.log(booleanIsLoggedIn);
 let someNumber = undefined
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//*********** Operations *****************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
